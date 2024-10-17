ffeb9ff4a0a0ad539994b2239c4221f959de821a
Rahul Raj <rahulraj.off@gmail.com>
2024-10-17 18:54:35 +0530

    Setup Backend

---
+++ Backend/src/test/java/com/rahul/todo_app/SimpleToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.todo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
